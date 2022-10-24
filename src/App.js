c7b79910abfdd57a7b9d527f89768a13b6525b19
Giordano <giordano.mendes94@gmail.com>
2022-10-24 19:04:23 -0300

    Routes and Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Provider from './Context/Provider';
+import DoneRecipes from './Pages/DoneRecipes';
+import Drink from './Pages/Drink';
+import FavoriteRecipes from './Pages/FavoriteRecipes';
+import Meals from './Pages/Meals';
+import Profile from './Pages/Profile';
 
-function App() {
+export default function App() {
   return (
     <Provider>
-      <h1>##VQV</h1>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/meals" component={ Meals } />
+        <Route path="/drinks" component={ Drink } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
     </Provider>
   );
 }
-
-export default App;
