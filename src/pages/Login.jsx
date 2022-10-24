513427a9dc400eab7a25ecefafb8efb25ece1e80
HigorMN <higor.maranhao2000@gmail.com>
2022-10-24 18:04:39 -0300

    Requisit 06 css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import logoRecipes from '../images/logoRecipes.png';
+import tomate from '../images/tomate.png';
 
 const PASSWORD_MIN = 6;
 
@@ -17,25 +19,35 @@ export default function Login(props) {
 
   return (
     <main>
-      <form onSubmit={ handleSubmit }>
+      <div className="header-login">
+        <img src={ logoRecipes } alt="Logo recipes" className="login-logo" />
+        <img src={ tomate } alt="Tomate" className="login-tomate" />
+      </div>
+      <form onSubmit={ handleSubmit } className="center login-form">
+        <h1 className="login-title">LOGIN</h1>
         <input
           type="email"
           value={ email }
           onChange={ ({ target: { value } }) => setEmail(value) }
           data-testid="email-input"
+          placeholder="Email"
+          className="login-input"
         />
         <input
           type="password"
           value={ senha }
           onChange={ ({ target: { value } }) => setSenha(value) }
           data-testid="password-input"
+          placeholder="Password"
+          className="login-input"
         />
         <button
           type="submit"
           disabled={ !(senha.length > PASSWORD_MIN && /\S+@\S+\.\S+/.test(email)) }
           data-testid="login-submit-btn"
+          className="login-button"
         >
-          Entrar
+          ENTER
         </button>
       </form>
     </main>
