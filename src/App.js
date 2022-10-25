0208cc0739548b20b18b8d84280454fff0350bc2
HigorMN <higor.maranhao2000@gmail.com>
2022-10-25 16:20:53 -0300

    criando rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Recipes from './components/Recipes';
 import Footer from './components/Footer';
 import Provider from './Context/Provider';
 import Login from './pages/Login';
@@ -12,15 +11,12 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/drinks">
-          <Recipes />
           <Footer />
         </Route>
         <Route exact path="/meals">
-          <Recipes />
           <Footer />
         </Route>
         <Route exact path="/profile">
-          <Recipes />
           <Footer />
         </Route>
       </Switch>
