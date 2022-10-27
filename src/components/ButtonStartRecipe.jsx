0520f72d6ab7b6ecc0d7095d9a38b9f8a5e134b9
HigorMN <higor.maranhao2000@gmail.com>
2022-10-27 10:01:50 -0300

    Mudando logica botao StartRecipe

--- src/components/ButtonStartRecipe.jsx
+++ src/components/ButtonStartRecipe.jsx
@@ -1,38 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './ButtonStartRecipe.css';
 
-export default function ButtonStartRecipe(props) {
-  const [btn, setBtn] = useState(true);
-  const [btnMsg, setBtnMsg] = useState(false);
+export default function ButtonStartRecipe() {
+  // const { id } = props;
+  // const localDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  // const localInProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
 
-  const { id } = props;
-  const localDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-  const localInProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
+  // if (localDoneRecipes !== null) {
+  //   const findId = localDoneRecipes.filter((e) => e.id === id).length > 0;
+  //   setBtn(findId);
+  // }
 
-  if (localDoneRecipes !== null) {
-    const findId = localDoneRecipes.filter((e) => e.id === id).length > 0;
-    setBtn(findId);
-  }
+  // if (localInProgressRecipes !== null) {
+  //   const findIdDrinks = Object.keys(localInProgressRecipes.drinks).includes(id);
+  //   const findIdMeals = Object.keys(localInProgressRecipes.meals).includes(id);
 
-  if (localInProgressRecipes !== null) {
-    const findIdDrinks = Object.keys(localInProgressRecipes.drinks).includes(id);
-    const findIdMeals = Object.keys(localInProgressRecipes.meals).includes(id);
-
-    setBtnMsg(findIdDrinks || findIdMeals);
-  }
+  //   setBtnMsg(findIdDrinks || findIdMeals);
+  // }
 
   return (
     <div>
-      {btn && (
-        <button
-          type="button"
-          data-testid="start-recipe-btn"
-          className="start-recipe-btn"
-        >
-          {btnMsg ? 'Continue Recipe' : 'Start Recipe' }
-        </button>
-      )}
+
+      <button
+        type="button"
+        data-testid="start-recipe-btn"
+        className="start-recipe-btn"
+      >
+        Start Recipe
+      </button>
     </div>
   );
 }
