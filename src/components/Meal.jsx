862bb670c806720d4c4b515976793f22bf27e0f2
HigorMN <higor.maranhao2000@gmail.com>
2022-11-01 14:40:20 -0300

    css Meal feito

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from '../Context/Context';
 import './MealsAndDrick.css';
+import './Buttons-favorite-share.css';
 import RecommendationDrink from './RecommendationDrink';
 import ButtonStartRecipe from './ButtonStartRecipe';
 import ButtonFavoriteRecipe from './ButtonFavoriteRecipe';
 import ButtonShareRecipe from './ButtonShareRecipe';
+import MealDetailsHeader from '../images/MealDetailsHeader.svg';
 
 export default function Meal(props) {
   const [mealVideoId, setMealVideoId] = useState('');
@@ -13,7 +15,7 @@ export default function Meal(props) {
   const [mealMeasure, setMealMeasure] = useState([]);
 
   const { apiMeal, setApiMeal, setTypeRecipe } = useContext(Context);
-  const { props: { match: { params: { id }, url } } } = props;
+  const { props: { match: { params: { id } } } } = props;
   useEffect(() => {
     const requestApi = async () => {
       const request = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
@@ -43,6 +45,18 @@ export default function Meal(props) {
   return (
     <>
       <header className="Recipe-header">
+        <nav className="Btns__share__favorite__container">
+          <div className="Recipe__category__container">
+            <img src={ MealDetailsHeader } alt="Drink Details Header" />
+            <p data-testid="recipe-category" className="recipe-category">
+              {apiMeal.strCategory}
+            </p>
+          </div>
+          <div className="Btns__container">
+            <ButtonFavoriteRecipe />
+            <ButtonShareRecipe />
+          </div>
+        </nav>
         <img
           src={ apiMeal.strMealThumb }
           alt={ apiMeal.strMeal }
@@ -54,9 +68,6 @@ export default function Meal(props) {
         </h1>
       </header>
       <main className="Foods-recipe-main">
-        <h3 data-testid="recipe-category" className="recipe-category">
-          {apiMeal.strCategory}
-        </h3>
         <h1 className="recipe-titles">Ingredients</h1>
         <div className="Foods-recipe-ingredient">
           <ul>
@@ -69,10 +80,6 @@ export default function Meal(props) {
               </li>
             ))}
           </ul>
-          <div className="btns-share-favorite">
-            <ButtonFavoriteRecipe />
-            <ButtonShareRecipe url={ url } />
-          </div>
         </div>
         <h1 className="recipe-titles">Instructions</h1>
         <div className="Foods-recipe-instructions">
