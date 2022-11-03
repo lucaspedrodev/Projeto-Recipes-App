2f1a121b647ad47057d2166177639d69e3f73a04
HigorMN <higor.maranhao2000@gmail.com>
2022-11-03 19:49:41 -0300

    Css profile concluido

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
+import './Profile.css';
+
+import doneIcon from '../images/doneIcon.svg';
+import favoriteIcon from '../images/favoriteIcon.svg';
+import logoutIcon from '../images/logoutIcon.svg';
+
 export default function Profile() {
   const [validEmail, setValidEmail] = useState('');
 
@@ -32,13 +38,9 @@ export default function Profile() {
   return (
     <>
       <Header />
-      <div>
+      <div className="Profile__container__email">
         <span data-testid="profile-email">
-          email:
-          {' '}
-          {
-            validEmail
-          }
+          {validEmail}
         </span>
       </div>
       <div>
@@ -46,33 +48,33 @@ export default function Profile() {
           data-testid="profile-done-btn"
           type="button"
           onClick={ () => handleBtnDoneRecipes() }
+          className="Profile__button"
         >
-          Done Recipes
-
+          <img src={ doneIcon } alt="doneIcon" />
+          <p>Done Recipes</p>
         </button>
-
       </div>
       <div>
         <button
           data-testid="profile-favorite-btn"
           type="button"
           onClick={ () => handleBtnFavoriteRecipes() }
+          className="Profile__button"
         >
+          <img src={ favoriteIcon } alt="favoriteIcon" />
           Favorite Recipes
-
         </button>
-
       </div>
       <div>
         <button
           type="button"
           data-testid="profile-logout-btn"
           onClick={ () => handleBtnLogout() }
+          className="Profile__button"
         >
-          Logout
-
+          <img src={ logoutIcon } alt="logoutIcon" />
+          <p>Logout</p>
         </button>
-
       </div>
     </>
   );
