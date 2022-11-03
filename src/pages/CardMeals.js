28db814e3231e32f7a060c45425aff7a499d792e
HigorMN <higor.maranhao2000@gmail.com>
2022-11-03 17:26:21 -0300

    Css CardDrink concluido

--- src/pages/CardMeals.js
+++ src/pages/CardMeals.js
@@ -1,20 +1,30 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 import Context from '../Context/Context';
 
 export default function CardMeals() {
   const { recipesData } = useContext(Context);
   return (
-    <main>
+    <div className="Recipes__container__card">
       {recipesData.map((recipe, index) => (
-        <div key={ recipe.idMeal } data-testid={ `${index}-recipe-card` }>
-          <img
-            data-testid={ `${index}-card-img` }
-            src={ recipe.strMealThumb }
-            alt={ recipe.strMeal }
-          />
-          <p data-testid={ `${index}-card-name` }>{recipe.strMeal}</p>
-        </div>
+        <Link
+          key={ recipe.idMeal }
+          to={ `/meals/${recipe.idMeal}` }
+          className="Recipe__card__name"
+        >
+          <div data-testid={ `${index}-recipe-card` } className="Recipe__card">
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ recipe.strMealThumb }
+              alt={ recipe.strMeal }
+              className="Recipe__img"
+            />
+            <div className="Recipe__card__name__container">
+              <p data-testid={ `${index}-card-name` }>{recipe.strMeal}</p>
+            </div>
+          </div>
+        </Link>
       ))}
-    </main>
+    </div>
   );
 }
